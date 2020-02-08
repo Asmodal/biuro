799c2012c69b1e45f01b46e445d4273410e39003
Kamil Labedzki <kamil.labedzki@wp.pl>
2020-02-08 12:01:25 +0100

    Initial commit

---
+++ src/main/java/pl/sda/biuro/BiuroApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.biuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiuroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiuroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/biuro/BiuroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.biuro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiuroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
